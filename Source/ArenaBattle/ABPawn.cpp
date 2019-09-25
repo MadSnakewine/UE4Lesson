511067a5d6bed76fc2e730d19ba1083f86023755
Culter <we13245@gmail.com>
2019-09-25 15:55:00 +0900

    5주차

--- Source/ArenaBattle/ABPawn.cpp
+++ Source/ArenaBattle/ABPawn.cpp
@@ -35,6 +35,14 @@ AABPawn::AABPawn()
 	{
 		Mesh->SetSkeletalMesh(SK_CARDBOARD.Object);
 	}
+
+	Mesh->SetAnimationMode(EAnimationMode::AnimationBlueprint);
+
+	static ConstructorHelpers::FClassFinder<UAnimInstance> WARRIOR_ANIM(TEXT("/Game/Book/Animation/WarriorAnimBlueprint.WarriorAnimBlueprint_C"));
+	if (WARRIOR_ANIM.Succeeded())
+	{
+		Mesh->SetAnimInstanceClass(WARRIOR_ANIM.Class);
+	}
 }
 
 // Called when the game starts or when spawned
